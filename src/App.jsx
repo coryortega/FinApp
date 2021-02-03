78542c5079f9b23895279628d277153a5deb0255
Cory Ortega <coryortega@Corys-MacBook-Pro.local>
2021-02-02 18:44:57 -0800

    initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 function App() {
+  const initialUser = {
+    name: "",
+    age: 0,
+    income: 0,
+    rent: 0,
+    funBudget: 0,
+    savings: 0,
+    expenses: [
+      {creditCard: 0},
+      {groceries: 0},
+      {loans: 0}
+    ]
+
+  };
+
+  const [user, setUser] = useState(initialUser);
+
   return (
     <div className='App'>
       <h1>Hello</h1>
