a939ba9be44bc1700ebca851cf92ad39155de3bf
Cory Ortega <coryortega@Corys-MacBook-Pro.local>
2021-02-03 17:24:46 -0800

    light styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './sass/_cory.scss'
 
 function App() {
   const initialUser = {
@@ -7,6 +8,7 @@ function App() {
     income: 0,
     funBudget: 0,
     savings: 0,
+    status:'',
     expenses: {
       rent: 0,
       creditCard: 0,
@@ -22,11 +24,11 @@ function App() {
     const savingRatio = user.savings / user.income;
 
     if(savingRatio < 0.1) {
-      alert(`Hey ${user.name}, your financial state is poor because you're not saving enough.`)
+      setUser({...user, status: 'poor'})
     } else if (savingRatio >= 0.1 && savingRatio < 0.2) {
-      alert(`Hey ${user.name}, your financial state is pretty good. However, you could save a little more`)
+      setUser({...user, status: 'good'})
     } else {
-      alert(`Hey ${user.name}, your financial state is great! You're saving 20% or more.`)
+      setUser({...user, status: 'great'})
     }
   }
 
@@ -34,74 +36,82 @@ function App() {
     <div className='App'>
       <h1>FinApp 4 Lief</h1>
       <form onSubmit={handleSubmit}>
-      <h3>General information: </h3>
-      <label>
-        Name:
-        <input
-          type="text"
-          value={user.name}
-          onChange={e => setUser({...user, name: e.target.value})}
-        />
-      </label>
-      <label>
-        Age:
-        <input
-          type="number"
-          value={user.age}
-          onChange={e => setUser({...user, age: e.target.value})}
-        />
-      </label>
-      <label>
-        Income:
-        <input
-          type="number"
-          value={user.income}
-          onChange={e => setUser({...user, income: e.target.value})}
-        />
-      </label>
-      <label>
-        Savings:
-        <input
-          type="number"
-          value={user.savings}
-          onChange={e => setUser({...user, savings: e.target.value})}
-        />
-      </label>
-      <h3>Monthly expenses: </h3>
-      <label>
-        Rent:
-        <input
-          type="number"
-          value={user.expenses.rent}
-          onChange={e => setUser({...user, expenses: {...user.expenses, rent: e.target.value}})}
-        />
-      </label>
-      <label>
-        Credit card:
-        <input
-          type="number"
-          value={user.expenses.creditCard}
-          onChange={e => setUser({...user, expenses: {...user.expenses, creditCard: e.target.value}})}
-        />
-      </label>
-      <label>
-        Groceries:
-        <input
-          type="number"
-          value={user.expenses.groceries}
-          onChange={e => setUser({...user, expenses: {...user.expenses, groceries: e.target.value}})}
-        />
-      </label>
-      <label>
-        Loans:
-        <input
-          type="number"
-          value={user.expenses.loans}
-          onChange={e => setUser({...user, expenses: {...user.expenses, loans: e.target.value}})}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+        <div className="general-info">
+          <h3>General information: </h3>
+          <div className="input-container">
+          <label>
+            Name:
+            <input
+              type="text"
+              value={user.name}
+              onChange={e => setUser({...user, name: e.target.value})}
+            />
+          </label>
+          <label>
+            Age:
+            <input
+              type="number"
+              value={user.age}
+              onChange={e => setUser({...user, age: e.target.value})}
+            />
+          </label>
+          <label>
+            Monthly income:
+            <input
+              type="number"
+              value={user.income}
+              onChange={e => setUser({...user, income: e.target.value})}
+            />
+          </label>
+          <label>
+            Monthly savings:
+            <input
+              type="number"
+              value={user.savings}
+              onChange={e => setUser({...user, savings: e.target.value})}
+            />
+          </label>
+          </div>
+        </div>
+        <div className="expenses-info">
+          <h3>Monthly expenses: </h3>
+          <div className="input-container">
+            <label>
+              Rent:
+              <input
+                type="number"
+                value={user.expenses.rent}
+                onChange={e => setUser({...user, expenses: {...user.expenses, rent: e.target.value}})}
+              />
+            </label>
+            <label>
+              Credit card:
+              <input
+                type="number"
+                value={user.expenses.creditCard}
+                onChange={e => setUser({...user, expenses: {...user.expenses, creditCard: e.target.value}})}
+              />
+            </label>
+            <label>
+              Groceries:
+              <input
+                type="number"
+                value={user.expenses.groceries}
+                onChange={e => setUser({...user, expenses: {...user.expenses, groceries: e.target.value}})}
+              />
+            </label>
+            <label>
+              Loans:
+              <input
+                type="number"
+                value={user.expenses.loans}
+                onChange={e => setUser({...user, expenses: {...user.expenses, loans: e.target.value}})}
+              />
+            </label>
+          </div>
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
   );
 }
